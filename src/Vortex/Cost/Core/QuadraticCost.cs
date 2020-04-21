2fe22108e64da8a7e21d5df4928d2601740b4cf6
Nirex <nirex.0@gmail.com>
2020-04-22 01:13:42 +0330

    Cost function implemented

---
+++ src/Vortex/Cost/Core/QuadraticCost.cs
@@ -0,0 +1,53 @@
+﻿// Copyright © 2020 Void-Intelligence All Rights Reserved.
+
+using System;
+using Vortex.Cost.Utility;
+using Vortex.Regularization.Utility;
+using Nomad.Matrix;
+
+namespace Vortex.Cost
+{
+    /// <summary>
+    /// "Quadratic Cost": Also known as "Mean Squared Error" or "Maximum Likelihood" or "Sum Squared Error"
+    /// </summary>
+    public class QuadraticCost : Utility.Cost
+    {
+        public QuadraticCost(QuadraticCostSettings settings) : base(settings) { }
+        
+        public override double Forward(Matrix Actual, Matrix Expected, int layerCount)
+        {
+            double error = 0.0;
+
+            for (int i = 0; i < Actual.Rows; i++)
+            {
+                for (int j = 0; j < Actual.Columns; j++)
+                {
+                    error += Math.Pow((Actual[i, j] - Expected[i, j]), 2);
+                }
+            }
+
+            error /= 2;
+
+            BatchCost += error;
+
+            return error;
+        }
+
+        public override Matrix Backward(Matrix Actual, Matrix Expected, int layerCount)
+        {
+            return Actual - Expected;
+        }
+
+        public override string ToString()
+        {
+            return Type().ToString();
+        }
+
+        public override ECostType Type()
+        {
+            return ECostType.QuadraticCost;
+        }
+    }
+
+    public class QuadraticCostSettings : CostSettings { }
+}
---
+++ src/Vortex/Cost/Utility/Cost.cs
@@ -0,0 +1,31 @@
+﻿// Copyright © 2020 Void-Intelligence All Rights Reserved.
+
+using System;
+using Vortex.Regularization;
+using Vortex.Regularization.Utility;
+using Nomad.Matrix;
+
+namespace Vortex.Cost.Utility
+{
+    /// <summary>
+    /// A cost function is a measure of "how good" a neural network did with respect to it's given training sample and the expected output. 
+    /// It also may depend on variables such as weights and biases.
+    /// A cost function is a single value, not a vector, because it rates how good the neural network did as a whole.
+    /// </summary>
+    public abstract class Cost
+    {
+        public Cost(CostSettings settings) { }
+        
+        public abstract double Forward(Matrix Actual, Matrix Expected, int layerCount);
+
+        public abstract Matrix Backward(Matrix Actual, Matrix Expected, int layerCount);
+
+        public abstract ECostType Type();
+
+        public double BatchCost { get; protected set; }
+
+        public virtual void ResetCost() { BatchCost = 0; }
+    }
+
+    public abstract class CostSettings { }
+}

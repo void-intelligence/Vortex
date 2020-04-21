047edf9abb21085329fb3055053c4081fcb82159
Nirex <nirex.0@gmail.com>
2020-04-22 01:20:58 +0330

    Layer placeholder

---
+++ src/Vortex/Layer/Core/FullyConnected.cs
@@ -0,0 +1,11 @@
+﻿// Copyright © 2020 Void-Intelligence All Rights Reserved.
+
+using Vortex.Layer.Utility;
+using Nomad.Matrix;
+
+namespace Vortex.Layer
+{
+    public class FullyConnected : Layer.Utility.Layer
+    {
+    }
+}
---
+++ src/Vortex/Layer/Utility/Layer.cs
@@ -0,0 +1,22 @@
+﻿// Copyright © 2020 Void-Intelligence All Rights Reserved.
+
+using System;
+using Vortex.Activation;
+using Vortex.Activation.Utility;
+using Vortex.Regularization;
+using Vortex.Regularization.Utility;
+using Nomad.Matrix;
+
+namespace Vortex.Layer.Utility
+{
+    /// <summary>
+    /// Base of all Layer classes
+    /// </summary>
+    public abstract class Layer
+    {
+    }
+
+    public class LayerSettings
+    {
+    }
+}

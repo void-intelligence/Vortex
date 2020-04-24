473d955377368a67c637900a3194efca710d3683
Nirex <nirex.0@gmail.com>
2020-04-24 13:43:06 +0330

    Implemented regularization and optimization on FC

--- src/Vortex/Layer/Core/FullyConnected.cs
+++ src/Vortex/Layer/Core/FullyConnected.cs
@@ -4,39 +4,48 @@ using Vortex.Layer.Utility;
 using Nomad.Matrix;
 using Vortex.Activation.Utility;
 using Vortex.Regularization.Utility;
+using Vortex.Optimizer.Utility;
 
 namespace Vortex.Layer
 {
     public class FullyConnected : BaseLayer
     {
-        public FullyConnected(LayerSettings settings) 
-            : base(settings)
+        public FullyConnected(LayerSettings settings, BaseOptimizer optimizer) 
+            : base(settings, optimizer)
         {
         }
 
         public override Matrix Forward(Matrix inputs)
         {
+            // Calculate Regularization Value
+            RegularizationValue = (float)RegularizationFunction.CalculateNorm(Params["W"]);
+
+            // Calculate Feed Forward Operation
             Params["X"] = inputs;
             Params["Z"] = (Params["W"].T() * Params["X"]) + Params["B"];
             Params["A"] = ActivationFunction.Forward(Params["Z"]);
             return Params["A"];
         }
 
-        public override Matrix Backward(Matrix dA, Matrix prevA)
+        public override Matrix Backward(Matrix dA)
         {
+            // Note: Params["A-1"] will be set in Network.Backward() function
             Grads["DA"] = dA;
             Grads["G'"] = ActivationFunction.Backward(Params["Z"]);
             Grads["DZ"] = Grads["DA"].Hadamard(Grads["G'"]);
-            Grads["DW"] = Grads["DZ"] * prevA;
+            Grads["DW"] = Grads["DZ"] * Params["A-1"];
             Grads["DB"] = Grads["DZ"];
             Grads["DA-1"] = Params["W"].T() * Grads["DZ"];
             return Grads["DA-1"];
         }
 
-        public override void Optimize(Matrix wGrad, Matrix bGrad)
+        public override void Optimize()
         {
-            Params["W"] = Params["W"] - wGrad;
-            Params["B"] = Params["B"] - bGrad;
+            Matrix deltaW = OptimizerFunction.CalculateDeltaW(Params["W"], Grads["DW"]);
+            Matrix deltaB = OptimizerFunction.CalculateDeltaB(Params["B"], Grads["DB"]);
+
+            Params["W"] = Params["W"] - deltaW;
+            Params["B"] = Params["B"] - deltaB;
         }
     }
 
--- src/Vortex/Layer/Utility/BaseLayer.cs
+++ src/Vortex/Layer/Utility/BaseLayer.cs
@@ -7,6 +7,7 @@ using Vortex.Regularization;
 using Vortex.Regularization.Utility;
 using Nomad.Matrix;
 using System.Collections.Generic;
+using Vortex.Optimizer.Utility;
 
 namespace Vortex.Layer.Utility
 {
@@ -15,15 +16,21 @@ namespace Vortex.Layer.Utility
     /// </summary>
     public abstract class BaseLayer
     {
+        public float RegularizationValue { get; protected set; }
         public LayerSettings Settings { get; private set; }
         public int NeuronCount { get; private set; }
         public BaseActivation ActivationFunction { get; private set; }
         public BaseRegularization RegularizationFunction { get; private set; }
+        public BaseOptimizer OptimizerFunction { get; private set; }
         public Dictionary<string, Matrix> Params { get; private set; }
         public Dictionary<string, Matrix> Grads { get; private set; }
 
-        public BaseLayer(LayerSettings layerSettings)
+        public BaseLayer(LayerSettings layerSettings, BaseOptimizer optimizer)
         {
+            RegularizationValue = 0.0f;
+
+            OptimizerFunction = optimizer;
+
             Params = new Dictionary<string, Matrix>();
 
             Grads = new Dictionary<string, Matrix>();
@@ -31,12 +38,10 @@ namespace Vortex.Layer.Utility
             Settings = layerSettings;
 
             NeuronCount = Settings.NeuronCount;
-
+            
             // Activation Setup
-            // Regularization Setup
             ActivationFunction = (Settings.ActivationFunction.Type()) switch
             {
-                EActivationType.Invalid => null,
                 EActivationType.Arctan => new Arctan(),
                 EActivationType.BinaryStep => new BinaryStep(),
                 EActivationType.BipolarSigmoid => new BipolarSigmoid(),
@@ -60,7 +65,6 @@ namespace Vortex.Layer.Utility
             // Regularization Setup
             RegularizationFunction = (Settings.RegularizationFunction.Type()) switch
             {
-                ERegularizationType.Invalid => null,
                 ERegularizationType.None => new Regularization.None(),
                 ERegularizationType.L1 => new Regularization.L1((L1Settings)Settings.RegularizationFunction),
                 ERegularizationType.L2 => new Regularization.L2((L2Settings)Settings.RegularizationFunction),
@@ -72,10 +76,10 @@ namespace Vortex.Layer.Utility
         public abstract Matrix Forward(Matrix inputs);
 
         // All Layer Backward Calculations
-        public abstract Matrix Backward(Matrix dA, Matrix prevA);
+        public abstract Matrix Backward(Matrix dA);
 
-        // All Layer Backward Calculations
-        public abstract void Optimize(Matrix wGrad, Matrix bGrad);
+        // All Layer Optimization Calculations
+        public abstract void Optimize();
     }
 
     public class LayerSettings

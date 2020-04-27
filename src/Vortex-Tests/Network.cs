355bdc09439f05590fa82d9545d42e7feb095ee5
Nirex <nirex.0@gmail.com>
2020-04-27 13:23:38 +0330

    Network tests for Forward and Backward

--- src/Vortex-Tests/Network.cs
+++ src/Vortex-Tests/Network.cs
@@ -3,12 +3,57 @@ using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Text;
 using Nomad.Matrix;
+using Vortex.Activation;
+using Vortex.Cost;
+using Vortex.Layer.Utility;
+using Vortex.Network;
+using Vortex.Optimizer;
+using Vortex.Regularization;
 
-namespace Vortex_Tests
+namespace VortexTests
 {
     [TestClass]
     public class VortexNetwork
     {
+        [TestMethod]
+        public void SequentualForwardTest()
+        {
+            Network network = new Network(new CrossEntropyCostSettings(), new GradientDescentSettings(0.001));
+            network.CreateLayer(ELayerType.FullyConnected, 784, new SigmoidSettings(), new L2Settings(2));
+            network.CreateLayer(ELayerType.Dropout, 100, new SigmoidSettings(), new L2Settings(2));
+            network.CreateLayer(ELayerType.FullyConnected, 100, new SigmoidSettings(), new L2Settings(2));
+            network.CreateLayer(ELayerType.Dropout, 100, new SigmoidSettings(), new L2Settings(2));
+            network.CreateLayer(ELayerType.Output, 10, new SigmoidSettings(), new L2Settings(2));
+            network.InitNetwork();
 
+            Matrix x = new Matrix(784, 1);
+            x.InFlatten();
+
+            Matrix y = new Matrix(10, 1);
+            y.InRandomize();
+
+            network.Forward(x, y);
+        }
+
+        [TestMethod]
+        public void SequentualBackwardTest()
+        {
+            Network network = new Network(new CrossEntropyCostSettings(), new GradientDescentSettings(0.001));
+            network.CreateLayer(ELayerType.FullyConnected, 784, new SigmoidSettings(), new L2Settings(2));
+            network.CreateLayer(ELayerType.Dropout, 100, new SigmoidSettings(), new L2Settings(2));
+            network.CreateLayer(ELayerType.FullyConnected, 100, new SigmoidSettings(), new L2Settings(2));
+            network.CreateLayer(ELayerType.Dropout, 100, new SigmoidSettings(), new L2Settings(2));
+            network.CreateLayer(ELayerType.Output, 10, new SigmoidSettings(), new L2Settings(2));
+            network.InitNetwork();
+
+            Matrix x = new Matrix(784, 1);
+            x.InFlatten();
+
+            Matrix y = new Matrix(10, 1);
+            y.InRandomize();
+
+            network.Forward(x, y);
+            network.Backward();
+        }
     }
 }

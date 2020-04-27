50f0b540ea21b9952a9cbfcfc4a3ef4c717f8906
Nirex <nirex.0@gmail.com>
2020-04-27 16:38:13 +0330

    Add: RMSProp & Momentum Test

--- src/Vortex-Tests/Optimizer.cs
+++ src/Vortex-Tests/Optimizer.cs
@@ -12,12 +12,60 @@ namespace VortexTests
         [TestMethod]
         public void SGDTest()
         {
-            GradientDescent gd = new GradientDescent(new GradientDescentSettings(0.001));
+            GradientDescent gd = new GradientDescent();
             Matrix X = new Matrix(10, 10);
             Matrix dJdX = new Matrix(10, 10);
-            X.InRandomize();
-            dJdX.InRandomize();
-            gd.CalculateDeltaW(X, dJdX);
-        } 
+
+            X.InFill(100);
+
+            for (int i = 0; i < 100; i++)
+            {
+                dJdX.InRandomize();
+                Matrix deltas = gd.CalculateDeltaW(X, dJdX);
+                X -= deltas;
+            }
+
+            Assert.IsTrue(X.FrobeniusNorm() < X.Fill(100).FrobeniusNorm(), "SGD Successful");
+        }
+
+
+        [TestMethod]
+        public void MomentumTest()
+        {
+            Momentum momentum = new Momentum();
+            Matrix X = new Matrix(10, 10);
+            Matrix dJdX = new Matrix(10, 10);
+
+            X.InFill(100);
+
+            for (int i = 0; i < 100; i++)
+            {
+                dJdX.InRandomize();
+                Matrix deltas = momentum.CalculateDeltaW(X, dJdX);
+                X -= deltas;
+            }
+
+            Assert.IsTrue(X.FrobeniusNorm() < X.Fill(100).FrobeniusNorm(), "SGD Successful");
+        }
+
+        [TestMethod]
+        public void RMSPropTest()
+        {
+            Momentum rms = new Momentum();
+            Matrix X = new Matrix(10, 10);
+            Matrix dJdX = new Matrix(10, 10);
+
+            X.InFill(100);
+           
+
+            for (int i = 0; i < 100; i++)
+            {
+                dJdX.InRandomize();
+                Matrix deltas = rms.CalculateDeltaW(X, dJdX);
+                X -= deltas;
+            }
+
+            Assert.IsTrue(X.FrobeniusNorm() < X.Fill(100).FrobeniusNorm(), "SGD Successful");
+        }
     }
 }

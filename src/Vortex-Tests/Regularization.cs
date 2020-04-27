b95ff57c12a765c264d6800e3ac910e58b27f04f
Nirex <nirex.0@gmail.com>
2020-04-27 13:23:59 +0330

    Add regularization test for L1 and L2

--- src/Vortex-Tests/Regularization.cs
+++ src/Vortex-Tests/Regularization.cs
@@ -1,14 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Text;
 using Nomad.Matrix;
+using Vortex.Regularization;
 
-namespace Vortex_Tests
+namespace VortexTests
 {
     [TestClass]
     public class VortexRegularization
     {
+        [TestMethod]
+        public void L1Test()
+        {
+            double lambda = 2.0;
+            L1 reg = new L1(new L1Settings(lambda));
+            Matrix mat = new Matrix(10, 10);
+            mat.InRandomize();
 
+            Assert.IsTrue(Math.Abs(mat.AbsoluteNorm() * lambda - reg.CalculateNorm(mat)) < 0.1, "L1 Norm calculation successful");
+        }
+
+        [TestMethod]
+        public void L2Test()
+        {
+            double lambda = 2.0;
+            L2 reg = new L2(new L2Settings(lambda));
+            Matrix mat = new Matrix(10, 10);
+            mat.InRandomize();
+
+            Assert.IsTrue(Math.Abs(mat.FrobeniusNorm() * lambda - reg.CalculateNorm(mat)) < 0.1, "L1 Norm calculation successful");
+
+        }
     }
 }

16cbe167b908efa02bc0d5375c45a2c4d36bdf0c
Nirex <nirex.0@gmail.com>
2020-05-07 04:03:15 +0330

    Fix all layer functions

--- src/Vortex/Layer/Core/Output.cs
+++ src/Vortex/Layer/Core/Output.cs
@@ -19,37 +19,32 @@ namespace Vortex.Layer
         public override Matrix Forward(Matrix inputs)
         {
             // Calculate Regularization Value On W and B
-            RegularizationValue = (float)RegularizationFunction.CalculateNorm(Params["W"]) + (float)RegularizationFunction.CalculateNorm(Params["B"]);
+            RegularizationValue = (float)RegularizationFunction.CalculateNorm(Params["W"]);
 
             // Calculate Feed Forward Operation
             Params["X"] = inputs;
-            Params["Z"] = (Params["W"].T() * Params["X"]) + Params["B"];
+            Params["Z"] = Params["W"] * Params["X"] + Params["B"];
             Params["A"] = ActivationFunction.Forward(Params["Z"]);
             return Params["A"];
         }
 
-        public override Matrix Backward(Matrix dA)
+        public override Matrix Backward(Matrix error)
         {
-            // Note: Params["A-1"] will be set in Network.Backward() function
-            // Helper Grads
-            Grads["DA"] = dA;
+            Grads["DA"] = error;
             Grads["G'"] = ActivationFunction.Backward(Params["Z"]);
             Grads["DZ"] = Grads["DA"].Hadamard(Grads["G'"]);
-
-            // The Intended Grads
-            Grads["DW"] = Params["A-1"] * Grads["DZ"].T();
+            Grads["DW"] = (Grads["DZ"] * Params["X"].T());
             Grads["DB"] = Grads["DZ"];
-            Grads["DA-1"] = Params["W"] * Grads["DZ"];
-            return Grads["DA-1"];
+            return Grads["DZ"];
         }
 
         public override void Optimize()
         {
-            Matrix deltaW = OptimizerFunction.CalculateDeltaW(Params["W"].T(), Grads["DW"]);
+            Matrix deltaW = OptimizerFunction.CalculateDeltaW(Params["W"], Grads["DW"]);
             Matrix deltaB = OptimizerFunction.CalculateDeltaB(Params["B"], Grads["DB"]);
 
-            Params["W"] = Params["W"] - deltaW;
-            Params["B"] = Params["B"] - deltaB;
+            Params["W"] -= deltaW;
+            Params["B"] -= deltaB;
         }
 
         public override ELayerType Type() => ELayerType.Output;

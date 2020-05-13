1130ae3242b616e836c9ceb00c432072f2fd5f1d
Nirex <nirex.0@gmail.com>
2020-05-14 02:22:15 +0330

    Add tests for normalization classes

---
+++ src/Vortex-Tests/NormalizationTests.cs
@@ -0,0 +1,22 @@
+﻿// Copyright © 2020 Void-Intelligence All Rights Reserved.
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nomad.Matrix;
+using Vortex.Normalization.Kernels;
+
+namespace VortexTests
+{
+    [TestClass]
+    public class Normalization
+    {
+        [TestMethod]
+        public void NoNormalizationTest()
+        {
+            var a = new Matrix(5, 5);
+            a.InRandomize();
+            var norm = new NoNorm();
+            Assert.IsTrue(Math.Abs(a.FrobeniusNorm() - norm.Normalize(a).FrobeniusNorm()) < 0.1, norm.Type().ToString() + " Normalization!");
+        }
+    }
+}

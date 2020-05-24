21339a9235e2fe7b9bb5fe16498658a6ddf021c0
Nirex <nirex.0@gmail.com>
2020-05-24 15:53:27 +0430

    Update NetworkTests.cs

--- src/Vortex-Tests/NetworkTests.cs
+++ src/Vortex-Tests/NetworkTests.cs
@@ -163,8 +163,8 @@ namespace VortexTests
 
             // Create Network
             var net = new Sequential(new CategoricalCrossEntropy(), new Adam(0.3), null, 128);
-            net.CreateLayer(new Dense(784, new Relu()));
-            net.CreateLayer(new Dropout(128,0.2f, new Relu()));
+            net.CreateLayer(new Dense(784, new Identity()));
+            net.CreateLayer(new Dense(128, new Tanh()));
             net.CreateLayer(new Output(10, new Softmax()));
             net.InitNetwork();
 
